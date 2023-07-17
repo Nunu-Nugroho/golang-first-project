c05dc43d7207c792848544f74e4fd297b8aac2ea
nugroho <nugrohowahidf22@tsgitdev.com>
2023-07-17 10:04:56 +0700

    active port .env

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/Nunu-Nugroho/golang-first-project/models"
 	"github.com/Nunu-Nugroho/golang-first-project/controllers/productcontroller"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -16,10 +21,17 @@ func main() {
 	r.PUT("/api/product/:id", productcontroller.Update)
 	r.DELETE("/api/product", productcontroller.Delete)
 	
-	port := "3000" // Ganti dengan port yang diinginkan
+	// port := "1005" // Ganti dengan port yang diinginkan
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}	
+	port := os.Getenv("ACTIVE_PORT")
+	fmt.Println("Port:", port)
 
 	if err := r.Run(":" + port); err != nil {
 		// Handle error jika terjadi
 		panic(err)
 	}
+	
 }
